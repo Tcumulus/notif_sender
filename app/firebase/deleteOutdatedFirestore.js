5148afe22e8f1ea0175cfba883af62eb6b2bc571
Tcumulus <maarten.laureyssen@gmail.com>
2021-11-07 16:53:26 +0100

    Issue #2 fixed

--- app/firebase/deleteOutdatedFirestore.js
+++ app/firebase/deleteOutdatedFirestore.js
@@ -7,7 +7,7 @@ const deleteOutdatedFirestore = () => {
 
   useEffect(() => {
     blocks.forEach(block => {
-      if (new Date(block.time) < new Date() - 60000*30) {
+      if (new Date(block.time.seconds*1000) < new Date() - 60000*30) {
         const docRef = projectFirestore.collection("blocks").doc(block.id)
         docRef.delete()
       }
